2d718ea7790cd472f694fe7406af1cbd251034bd
Zunayed <zunayed@gmail.com>
2014-03-25 23:28:50 -0400

    new solution

--- p2.py
+++ p2.py
@@ -15,10 +15,22 @@ def solution_one(limit):
     return result
 
 
+def solution_two(limit):
+    x = y = 1
+    sum = 0
+    while (sum < limit):
+        sum += (x + y)
+        x, y = x + 2 * y, 2 * x + 3 * y
+    return sum
+
+
 class TestSolutions(unittest.TestCase):
 
     def test_solution_one(self):
         self.assertEqual(solution_one(144), 44)
+
+    def test_solution_two(self):
+        self.assertEqual(solution_two(144), 44)
 
 if __name__ == '__main__':
     unittest.main()
